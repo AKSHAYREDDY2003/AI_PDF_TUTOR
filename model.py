d6e1104d026024648cc83c5e8dafcc8ef7a04834
AKSHAYREDDY2003 <kommireddyakshayreddy@gmail.com>
2026-03-20 17:58:02 +0530

    Switch to Streamlit for API key management

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-import os
-
-api_key = os.getenv("GOOGLE_API_KEY")
+import streamlit as st
+api_key = st.secrets["GOOGLE_API_KEY"]
 model=ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite-preview',google_api_key=api_key)
