bb8926af00db91b24c12a831088f5b5072732953
AKSHAYREDDY2003 <kommireddyakshayreddy@gmail.com>
2026-03-20 17:28:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from embeddings import api_key
+import os
+
+api_key = os.getenv("GOOGLE_API_KEY")
 model=ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite-preview',google_api_key=api_key)
