26497d713d6846beb623a56c8ac28d6befe2c774
AKSHAYREDDY2003 <kommireddyakshayreddy@gmail.com>
2026-03-20 19:19:48 +0530

    Change API key retrieval method in model.py
    Updated the method of retrieving the API key from secrets.

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
-api_key = st.secrets["GOOGLE_API_KEY"]
+api_key = st.secrets.get("GOOGLE_API_KEY")
 model=ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite-preview',google_api_key=api_key)
