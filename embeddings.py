1eb7dcf9720b23d1804e06a567fcdd67ffc88d6a
AKSHAYREDDY2003 <kommireddyakshayreddy@gmail.com>
2026-05-01 20:02:11 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -2,6 +2,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 api_key = st.secrets.get("GOOGLE_API_KEY")
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="gemini-embedding-001",
+    model="models/embedding-001",
     google_api_key=api_key
 )
